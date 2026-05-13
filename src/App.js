b1a9e8d1919788805692b3e89be7c95f405b867a
Simha-raj <polireddyrajulanaidu123@gmail.com>
2026-05-13 16:40:09 +0530

    Add optional chaining to data length check

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {data.length > 0 ? (data.map((item, index) => (
+            {data?.length > 0 ? (data.map((item, index) => (
               <tr>
 
                 <td>{item.code}</td>
